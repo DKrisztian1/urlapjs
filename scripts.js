92ad3ee1a03361412cd3fffca72c192e63bfe70d
DKrisztian1 <71670029+DKrisztian1@users.noreply.github.com>
2022-11-02 17:44:01 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function visszaigazolas(){
+    document.getElementById("visszNev").innerHTML = document.getElementById("nev").value;
+    document.getElementById("visszCim").innerHTML = document.getElementById("cim").value;
+    document.getElementById("visszTel").innerHTML = document.getElementById("tel").value;
+}
